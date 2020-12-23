e6952ddc8ff9a0baf4aff0e37682cb3786961a3d
nakebuyi <pengpeng_231@126.com>
2020-12-23 19:34:58 +0800

    first commit

---
+++ apps/jumpserver/const.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+#
+import os
+
+from .conf import ConfigManager
+
+__all__ = ['BASE_DIR', 'PROJECT_DIR', 'VERSION', 'CONFIG', 'DYNAMIC']
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+PROJECT_DIR = os.path.dirname(BASE_DIR)
+VERSION = "v2.0.2"
+CONFIG = ConfigManager.load_user_config()
+DYNAMIC = ConfigManager.get_dynamic_config(CONFIG)
+
+
